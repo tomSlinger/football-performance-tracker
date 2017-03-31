931e865a023599b5dc9353fb378159b557a6182e
Tom Slinger <t_o_m_ouk@hotmail.com>
2017-03-31 13:29:07 +0100

    delete view finished

--- delete/index.php
+++ delete/index.php
@@ -15,6 +15,17 @@
                     <div class="row">
                         <div class="col-md-12 text-center">
                             <h1>Delete Game | ID: <?php echo $game_id; ?></h1>
+                            <hr />
+                        </div>
+                    </div>
+                    <div class="row text-center">
+                        <div class="col-md-12">
+                            <form class="" action="../controllers/DeleteGameController.php?id=<?php echo $game_id; ?>" method="post">
+                                <h3>Are you sure you want to delete this game?</h3>
+                                <p class="text-danger">Note: Once deleted, a game can NOT be restored.</p>
+                                <input type="submit" value="Yes" class="btn btn-danger btn-lg" />
+                                <a href="javascript:history.go(-1)" class="btn btn-success btn-lg">No</a>
+                            </form>
                         </div>
                     </div>
                 </div>
